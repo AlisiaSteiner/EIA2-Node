384b50184a384e7aad502b5bde6c3825b874fec2
Alistone <Alistone@Alistone-PC.fritz.box>
2017-06-25 17:39:33 +0200

    Anpassung auf A10

--- Aufgabe10/NodeTest.ts
+++ Aufgabe10/NodeTest.ts
@@ -32,7 +32,8 @@ function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerRes
 
     _response.setHeader("Access-Control-Allow-Origin", "*");
     _response.setHeader("content-type", "text/html; charset=utf-8");
-    _response.write("Ich h√∂re Stimmen!");
+    _response.write("Bestellung eingegangen");
+    _response.write(query["Normal - 1Ä je Kugel"]);
     _response.query;
     _response.end();
 }
